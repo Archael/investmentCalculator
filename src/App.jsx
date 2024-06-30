3e3e5fb5fe9f4d4827c8607f56e2d5196f6129e4
Adam Evans <adam3van5@gmail.com>
2024-06-30 12:46:53 +0900

    add results component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import Results from './components/Results';
 
 function App() {
   return (
     <>
       <Header />
       <UserInput />
-
-      <div id="result">
-        <div className="center">
-          <h2>Results</h2>
-        </div>
-      </div>
+      <Results />
     </>
   );
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,11 @@
+function Results() {
+  return (
+    <div id="result">
+      <div className="center">
+        <h2>Results...</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Results;
