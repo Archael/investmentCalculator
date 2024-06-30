f5b93415073bd7501a47dae034ea2d18380f24fc
Adam Evans <adam3van5@gmail.com>
2024-06-30 14:38:39 +0900

    calculate results data

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,7 @@
 import { calculateInvestmentResults, formatter } from '../util/investment';
 function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
+  const initialInvestment = input.initialInvestment;
   return (
     <div className="flex-center">
       <h2>Results</h2>
@@ -8,20 +9,29 @@ function Results({ input }) {
         <thead>
           <tr>
             <th>Year</th>
-            <th>Interest</th>
-            <th>Value at end of year</th>
-            <th>Annual investment</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
           </tr>
         </thead>
         <tbody>
-          {resultsData.map((result) => (
-            <tr key={result.year}>
-              <td>{result.year}</td>
-              <td>{formatter.format(result.interest)}</td>
-              <td>{formatter.format(result.valueEndOfYear)}</td>
-              <td>{formatter.format(result.annualInvestment)}</td>
-            </tr>
-          ))}
+          {resultsData.map((yearData) => {
+            const totalInterest =
+              yearData.valueEndOfYear -
+              yearData.annualInvestment * yearData.year -
+              initialInvestment;
+            const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+            return (
+              <tr key={yearData.year}>
+                <td>{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
